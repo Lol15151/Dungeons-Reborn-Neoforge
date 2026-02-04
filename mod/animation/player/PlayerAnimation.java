8b46b5c50bcb80af73f55cc714c029eb768c25a1
Lol15ius <arthurhayton1335@icloud.com>
2026-02-04 19:50:20 +0000

    Delete mod/animation directory

--- mod/animation/player/PlayerAnimation.java
+++
@@ -1,9 +0,0 @@
-package net.lol15.dungeonsreborn.mod.animation.player;
-
-import net.neoforged.bus.api.IEventBus;
-
-public interface PlayerAnimation {
-
-    // Used ONLY for mod bus events (keybind registration etc)
-    void registerClient(IEventBus modBus);
-}
